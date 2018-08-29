89e3527a753f0ecb6d71eb753d0db3e252899bed
Rhys Clay <rhys@theitteam.com.au>
2018-08-29 11:55:48 +1000

    Added check prior to launching siema.js

--- src/client.js
+++ src/client.js
@@ -1,44 +1,48 @@
 jQuery( function( $ ) {
-	const slider = new Siema( {
-		selector: '.siema',
-		loop: true,
-		onChange: onChangeCallback,
-		duration: 300,
-	} );
+	const siema = document.querySelector( '.siema' );
 
-	setInterval(
-		() => slider.next(),
-		parseInt( slider.selector.dataset.delay ) * 1000
-	);
-
-	function onChangeCallback() {
-		const btns = document.querySelectorAll( '.tautala-pagination-button' );
-		btns.forEach( ( btn, i ) => {
-			if ( btn.classList && this.currentSlide !== i ) {
-				btn.classList.remove( 'active' );
-			} else {
-				btn.classList.add( 'active' );
-			}
+	if ( siema ) {
+		const slider = new Siema( {
+			selector: siema,
+			loop: true,
+			onChange: onChangeCallback,
+			duration: 300,
 		} );
-	}
 
-	// Add a function that generates pagination to prototype
-	Siema.prototype.addPagination = function() {
-		for ( let i = 0; i < this.innerElements.length; i++ ) {
-			const btn = document.createElement( 'button' );
-			btn.className = 'tautala-pagination-button';
-			if ( i === 0 ) {
-				btn.className += ' active';
-			}
-			//btn.textContent = i;
-			btn.addEventListener( 'click', () => {
-				this.goTo( i );
+		setInterval(
+			() => slider.next(),
+			parseInt( slider.selector.dataset.delay ) * 1000
+		);
+
+		function onChangeCallback() {
+			const btns = document.querySelectorAll( '.tautala-pagination-button' );
+			btns.forEach( ( btn, i ) => {
+				if ( btn.classList && this.currentSlide !== i ) {
+					btn.classList.remove( 'active' );
+				} else {
+					btn.classList.add( 'active' );
+				}
 			} );
-			document.querySelector( '.tautala-pagination' ).appendChild( btn );
-			//this.selector.appendChild( btn );
 		}
-	};
 
-	// Trigger pagination creator
-	slider.addPagination();
+		// Add a function that generates pagination to prototype
+		Siema.prototype.addPagination = function() {
+			for ( let i = 0; i < this.innerElements.length; i++ ) {
+				const btn = document.createElement( 'button' );
+				btn.className = 'tautala-pagination-button';
+				if ( i === 0 ) {
+					btn.className += ' active';
+				}
+				//btn.textContent = i;
+				btn.addEventListener( 'click', () => {
+					this.goTo( i );
+				} );
+				document.querySelector( '.tautala-pagination' ).appendChild( btn );
+				//this.selector.appendChild( btn );
+			}
+		};
+
+		// Trigger pagination creator
+		slider.addPagination();
+	}
 } );
